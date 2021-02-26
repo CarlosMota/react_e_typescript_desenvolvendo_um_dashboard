7ff061a77f6f3b9bb9eddcd847a4dcc12ca19d79
Carlos Silva <carlos.motta89@mail.com>
2021-02-25 22:21:33 -0400

    [S3A12] Criando o header principal
    Signed-off-by: Carlos Silva <carlos.motta89@mail.com>

--- src/Components/MainHeader/index.tsx
+++ src/Components/MainHeader/index.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import { Container } from './styles';
+/** useMemo = é um hooks do react que  memoriza valores*/
+import React, {useMemo} from 'react';
+
+import emojis from '../../utils/emojis';
+
+import { 
+    Container,
+    Profile,
+    Welcome,
+    UserName
+} from './styles';
 
 const MainHeader: React.FC = () => {
+
+    const emoji = useMemo(() => {
+        const indice = Math.floor(Math.random() * emojis.length);
+        return emojis[indice];
+    },[]);
+
     return (
         <Container>
-            <h1>MainHeader</h1>
+            <h1>Toogle</h1>
+            <Profile>
+                <Welcome>Olá, {emoji}</Welcome>
+                <UserName>Rodrigo Gonçalves</UserName>
+            </Profile>
         </Container>
     );    
 }
